f83892a35e192411cbdfe7aa66add706b9f9f6b8
Melenium2 <osumelenium@gmail.com>
2020-06-18 16:46:57 +0500

    Change caller skip value again

--- config.go
+++ config.go
@@ -38,7 +38,7 @@ func (c *Config) TimePref(format string) {
 
 func (c *Config) CallerPref() {
 	c.Pref(func() interface{} {
-		_, file, row, _ := runtime.Caller(6)
+		_, file, row, _ := runtime.Caller(4)
 		idx := strings.LastIndexByte(file, '/')
 		return fmt.Sprintf("caller=%s:%d line", file[idx+1:], row)
 	})
